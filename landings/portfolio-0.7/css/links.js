2c2d3c5e094d0f62584aea4aa72ecd72e950ae46
Odys Zhou <odys.zhou@gmail.com>
2025-11-20 23:50:22 +0800

    :

--- landings/portfolio-0.7/css/links.js
+++ landings/portfolio-0.7/css/links.js
@@ -1,7 +1,7 @@
 links = {
-    "apk": "https://github.com/odys-z/semantic-jserv/releases/download/portfolio-synode-0.7.0/app-release.apk",
-    "synode-gui": "https://github.com/odys-z/semantic-jserv/releases/download/portfolio-synode-0.7.0/jserv-portfolio-0.7.1.zip",
-    "registry-demo": "https://github.com/odys-z/semantic-jserv/releases/download/portfolio-synode-0.7.0/registry.zip"
+    "apk": "https://github.com/odys-z/semantic-jserv/releases/download/portfolio-synode-0.7.0/portfolio-0.7.4.apk",
+    "synode-gui": "https://github.com/odys-z/semantic-jserv/releases/download/portfolio-synode-0.7.0/portfolio-synode-0.7.7.zip",
+    "registry-central": "https://github.com/odys-z/semantic-jserv/releases/download/portfolio-synode-0.7.0/registry.zip"
 }
 
 function setHref(aid, linkname) {
