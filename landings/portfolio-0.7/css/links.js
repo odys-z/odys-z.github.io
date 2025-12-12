7f3efbb073585f4df7c9966b66f0a657528af98b
Odys Zhou <odys.zhou@gmail.com>
2025-12-12 11:13:47 +0800

    .

--- landings/portfolio-0.7/css/links.js
+++ landings/portfolio-0.7/css/links.js
@@ -4,6 +4,22 @@ links = {
     "registry-central": "https://github.com/odys-z/semantic-jserv/releases/download/portfolio-synode-0.7.0/registry.zip"
 }
 
+function loadLinks(onRedirect) {
+    fetch('css/redirection.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log('Redirecting:', data.redirect);
+        fetch(data.redirect)
+            .then(response => response.json())
+            .then(data => {
+                console.log("links:", data);
+                onRedirect(data);
+            })
+            .catch(error => console.error('Error:', error));
+    })
+    .catch(error => console.error('Error:', error));
+}
+
 function setHref(aid, linkname) {
     document.getElementById(aid).href = links[linkname];
 }
